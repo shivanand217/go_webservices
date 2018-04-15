e403a782ce7e91f4e0c99eb148191ad77903b5a8
shivanand217 <apple@apples-MacBook-Air.local>
2018-04-15 18:19:44 +0530

    json response through go

---
+++ c1/1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type API struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *.httpRequest) {
+
+		message := API{"Hello, world!"}
+
+		output, err := json.Marshal(message)
+
+		if err != nil {
+			fmt.Println("Something went wrong!!")
+		}
+
+		fmt.Fprintf(w, string(output))
+	})
+
+	// Listen requests on port 8080
+	http.ListenAndServe(":8080", nil)
+}
