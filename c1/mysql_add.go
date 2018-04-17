e5911ea85a219128e65f5fdd89ae9f52a8c44e59
shiv anand <shivakp2111@gmail.com>
2018-04-17 11:14:11 +0530

    17-04-2018

--- c1/mysql_add.go
+++ c1/mysql_add.go
@@ -49,7 +49,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(q)
 }
 
-// getting the requested user from database
+// getting user with requested id from database
 func GetUser(w http.ResponseWriter, r *http.Request) {
 
 	// mux function Vars for getting the values
